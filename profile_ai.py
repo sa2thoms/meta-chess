ef5828c7a0704bf542f9c7534814a43efe292da8
Simon Thomson <simon.thomson@honeywell.com>
2018-04-20 15:01:51 -0400

    added profiler for AI

---
+++ profile_ai.py
@@ -0,0 +1,37 @@
+import pytest
+from RuleSet import RuleSet
+from Game import Game
+
+from pieces.Piece import Piece
+from pieces.Pawn import Pawn
+from pieces.King import King
+from pieces.Queen import Queen
+from pieces.Rook import Rook
+from pieces.Knight import Knight
+from pieces.Bishop import Bishop
+
+from Square import Square
+from Move import Move
+
+from NoRuleException import NoRuleException
+from IllegalMoveException import IllegalMoveException
+from InvalidMoveStringException import InvalidMoveStringException
+
+from Ai import Ai
+
+import NormalChessConfig
+
+import cProfile
+
+if __name__ == '__main__':
+    ruleSet = NormalChessConfig.ruleSet
+    def promotionCallback():
+        return 'q'
+    game = Game(ruleSet, promotionCallback)
+    game.load()
+    game.move('E2 to E4')
+
+    ai = Ai(2)
+
+    cProfile.run('ai.bestMove(game)')
+    
