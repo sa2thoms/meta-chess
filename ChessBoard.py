af6db9732a961bcaac97a075701755210966dbc1
Cheryl Thomson <cheryl47.thomson@gmail.com>
2018-05-09 11:57:49 -0400

    ChessBoard is a class instanced by PrintChessBoard

--- ChessBoard.py
+++ ChessBoard.py
@@ -33,68 +33,70 @@ class ResizingCanvas(Canvas):
 class ChessBoard:
 
     def __init__(self, master, myGame):
-        canvasSize = 800
 
+        self.master = master
+        self.myGame = myGame
+
+        canvasSize = 800
         colours = ("#EADDC5", "#BA893C")
         squares = 8
         ratio = 1/squares
-
         m = ratio*canvasSize
-        self.master = master
-        myframe = Frame(master)
-        myframe.pack(fill=BOTH, expand=YES)
+        
+        self.myframe = Frame(master)
+        self.myframe.pack(fill=BOTH, expand=YES)
 
-        w = ResizingCanvas(myframe, 
+        self.myCanvas = ResizingCanvas(self.myframe, 
                     width=canvasSize, 
                     height=canvasSize, highlightthickness=0)
-        w.pack(fill=BOTH, expand=YES)
+        self.myCanvas.pack(fill=BOTH, expand=YES)
 
-        blackKing = PhotoImage(file = "images/bK.gif")
-        blackQueen = PhotoImage(file = "images/bQ.gif")
-        blackBishop = PhotoImage(file = "images/bB.gif")
-        blackKnight = PhotoImage(file = "images/bN.gif")
-        blackRook = PhotoImage(file = "images/bR.gif")
-        blackPawn = PhotoImage(file = "images/bP.gif")
-        whiteKing = PhotoImage(file = "images/wK.gif")
-        whiteQueen = PhotoImage(file = "images/wQ.gif")
-        whiteBishop = PhotoImage(file = "images/wB.gif")
-        whiteKnight = PhotoImage(file = "images/wN.gif")
-        whiteRook = PhotoImage(file = "images/wR.gif")
-        whitePawn = PhotoImage(file = "images/wP.gif")
+        self.blackKing = PhotoImage(file = "images/bK.gif")
+        self.blackQueen = PhotoImage(file = "images/bQ.gif")
+        self.blackBishop = PhotoImage(file = "images/bB.gif")
+        self.blackKnight = PhotoImage(file = "images/bN.gif")
+        self.blackRook = PhotoImage(file = "images/bR.gif")
+        self.blackPawn = PhotoImage(file = "images/bP.gif")
+        self.whiteKing = PhotoImage(file = "images/wK.gif")
+        self.whiteQueen = PhotoImage(file = "images/wQ.gif")
+        self.whiteBishop = PhotoImage(file = "images/wB.gif")
+        self.whiteKnight = PhotoImage(file = "images/wN.gif")
+        self.whiteRook = PhotoImage(file = "images/wR.gif")
+        self.whitePawn = PhotoImage(file = "images/wP.gif")
 
         for i in range(squares):
             for j in range(squares):
                 fillchoice = colours[(i+j)%2]
-                w.create_rectangle(i*m, j*m,(i+1)*m,(j+1)*m, fill=fillchoice, tag="squares")
+                self.myCanvas.create_rectangle(i*m, j*m,(i+1)*m,(j+1)*m, fill=fillchoice, tag="squares")
                 thisPiece = myGame.getPiece(Square(i,j))
                 if (thisPiece):
                     if (thisPiece.color == 0):
                         if (thisPiece.symbol == 'pa'):
-                            w.create_image(i*m+m/2, j*m+m/2, image = blackPawn, tag = "bP")
+                            self.myCanvas.create_image(i*m+m/2, j*m+m/2, image = self.blackPawn, tag = "bP")
                         if (thisPiece.symbol == 'ro'):
-                            w.create_image(i*m+m/2, j*m+m/2, image = blackRook, tag="bR")
+                            self.myCanvas.create_image(i*m+m/2, j*m+m/2, image = self.blackRook, tag="bR")
                         if (thisPiece.symbol == 'kn'):
-                            w.create_image(i*m+m/2, j*m+m/2, image = blackKnight, tag="bN")
+                            self.myCanvas.create_image(i*m+m/2, j*m+m/2, image = self.blackKnight, tag="bN")
                         if (thisPiece.symbol == 'bi'):
-                            w.create_image(i*m+m/2, j*m+m/2, image = blackBishop, tag="bB")
+                            self.myCanvas.create_image(i*m+m/2, j*m+m/2, image = self.blackBishop, tag="bB")
                         if (thisPiece.symbol == 'Qu'):
-                            w.create_image(i*m+m/2, j*m+m/2, image = blackQueen, tag="bQ")
+                            self.myCanvas.create_image(i*m+m/2, j*m+m/2, image = self.blackQueen, tag="bQ")
                         if (thisPiece.symbol == 'Ki'):
-                            w.create_image(i*m+m/2, j*m+m/2, image = blackKing, tag="bK")
+                            self.myCanvas.create_image(i*m+m/2, j*m+m/2, image = self.blackKing, tag="bK")
                     if (thisPiece.color == 1):
                         if (thisPiece.symbol == 'pa'):
-                            w.create_image(i*m+m/2, j*m+m/2, image = whitePawn, tag = "bP")
+                            self.myCanvas.create_image(i*m+m/2, j*m+m/2, image = self.whitePawn, tag = "bP")
                         if (thisPiece.symbol == 'ro'):
-                            w.create_image(i*m+m/2, j*m+m/2, image = whiteRook, tag="bR")
+                            self.myCanvas.create_image(i*m+m/2, j*m+m/2, image = self.whiteRook, tag="bR")
                         if (thisPiece.symbol == 'kn'):
-                            w.create_image(i*m+m/2, j*m+m/2, image = whiteKnight, tag="bN")
+                            self.myCanvas.create_image(i*m+m/2, j*m+m/2, image = self.whiteKnight, tag="bN")
                         if (thisPiece.symbol == 'bi'):
-                            w.create_image(i*m+m/2, j*m+m/2, image = whiteBishop, tag="bB")
+                            self.myCanvas.create_image(i*m+m/2, j*m+m/2, image = self.whiteBishop, tag="bB")
                         if (thisPiece.symbol == 'Qu'):
-                            w.create_image(i*m+m/2, j*m+m/2, image = whiteQueen, tag="bQ")
+                            self.myCanvas.create_image(i*m+m/2, j*m+m/2, image = self.whiteQueen, tag="bQ")
                         if (thisPiece.symbol == 'Ki'):
-                            w.create_image(i*m+m/2, j*m+m/2, image = whiteKing, tag="bK")        
+                            self.myCanvas.create_image(i*m+m/2, j*m+m/2, image = self.whiteKing, tag="bK")        
                 
 
-        w.addtag_all("all")
+        self.myCanvas.addtag_all("all")
 
