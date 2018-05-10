2d0087cfa84c5d8a396c672569fe028dd6e0a4c3
Cheryl Thomson <cheryl47.thomson@gmail.com>
2018-05-10 10:54:02 -0400

    method for printing board and method for printing pieces

--- ChessBoard.py
+++ ChessBoard.py
@@ -33,69 +33,72 @@ class ChessBoard:
         self.master = master
         self.myGame = myGame
         self.colours = squareColors.SQUARE_COLORS
-      
-        def printSquares(self):
 
-            self.myframe = Frame(master)
-            self.myframe.pack(fill=BOTH, expand=YES)
+    def printSquares(self):
 
-            self.myCanvas = ResizingCanvas(self.myframe, 
-                        width=CANVAS_SIZE, 
-                        height=CANVAS_SIZE, highlightthickness=0)
-            self.myCanvas.pack(fill=BOTH, expand=YES)
+        M = self.M
 
-            self.blackKing = PhotoImage(file = pieceImages.bKImage)
-            self.blackQueen = PhotoImage(file = pieceImages.bQImage)
-            self.blackBishop = PhotoImage(file = pieceImages.bBImage)
-            self.blackKnight = PhotoImage(file = pieceImages.bNImage)
-            self.blackRook = PhotoImage(file = pieceImages.bRImage)
-            self.blackPawn = PhotoImage(file = pieceImages.bPImage)
-            self.whiteKing = PhotoImage(file = pieceImages.wKImage)
-            self.whiteQueen = PhotoImage(file = pieceImages.wQImage)
-            self.whiteBishop = PhotoImage(file = pieceImages.wBImage)
-            self.whiteKnight = PhotoImage(file = pieceImages.wNImage)
-            self.whiteRook = PhotoImage(file = pieceImages.wRImage)
-            self.whitePawn = PhotoImage(file = pieceImages.wPImage)
+        self.myframe = Frame(self.master)
+        self.myframe.pack(fill=BOTH, expand=YES)
 
-            for i in range(SQUARES):
-                for j in range(SQUARES):
-                    fillchoice = self.colours[(i+j)%2]
+        self.myCanvas = ResizingCanvas(self.myframe, 
+                        width=self.CANVAS_SIZE, 
+                        height=self.CANVAS_SIZE, highlightthickness=0)
+        self.myCanvas.pack(fill=BOTH, expand=YES)
+
+        self.blackKing = PhotoImage(file = pieceImages.bKImage)
+        self.blackQueen = PhotoImage(file = pieceImages.bQImage)
+        self.blackBishop = PhotoImage(file = pieceImages.bBImage)
+        self.blackKnight = PhotoImage(file = pieceImages.bNImage)
+        self.blackRook = PhotoImage(file = pieceImages.bRImage)
+        self.blackPawn = PhotoImage(file = pieceImages.bPImage)
+        self.whiteKing = PhotoImage(file = pieceImages.wKImage)
+        self.whiteQueen = PhotoImage(file = pieceImages.wQImage)
+        self.whiteBishop = PhotoImage(file = pieceImages.wBImage)
+        self.whiteKnight = PhotoImage(file = pieceImages.wNImage)
+        self.whiteRook = PhotoImage(file = pieceImages.wRImage)
+        self.whitePawn = PhotoImage(file = pieceImages.wPImage)
+
+        for i in range(self.SQUARES):
+            for j in range(self.SQUARES):
+                fillchoice = self.colours[(i+j)%2]
                 self.myCanvas.create_rectangle(i*M, j*M,(i+1)*M,(j+1)*M, fill=fillchoice, tag="squares")
 
-            self.myCanvas.addtag_all("all")
+        self.myCanvas.addtag_all("all")
 
 
-        def mapPieces(self):
+    def mapPieces(self):
 
-            for i in range(SQUARES):
-                for j in range(SQUARES):
-                    thisPiece = myGame.getPiece(Square(i,j))
-                    if (thisPiece):
-                        if (thisPiece.color == 0):
-                            if (thisPiece.symbol == 'pa'):
-                                self.myCanvas.create_image(i*M+M/2, j*M+M/2, image = self.blackPawn, tag = "bP")
-                            if (thisPiece.symbol == 'ro'):
-                                self.myCanvas.create_image(i*M+M/2, j*M+M/2, image = self.blackRook, tag="bR")
-                            if (thisPiece.symbol == 'kn'):
-                                self.myCanvas.create_image(i*M+M/2, j*M+M/2, image = self.blackKnight, tag="bN")
-                            if (thisPiece.symbol == 'bi'):
-                                self.myCanvas.create_image(i*M+M/2, j*M+M/2, image = self.blackBishop, tag="bB")
-                            if (thisPiece.symbol == 'Qu'):
-                                self.myCanvas.create_image(i*M+M/2, j*M+M/2, image = self.blackQueen, tag="bQ")
-                            if (thisPiece.symbol == 'Ki'):
-                                self.myCanvas.create_image(i*M+M/2, j*M+M/2, image = self.blackKing, tag="bK")
-                        if (thisPiece.color == 1):
-                            if (thisPiece.symbol == 'pa'):
-                                self.myCanvas.create_image(i*M+M/2, j*M+M/2, image = self.whitePawn, tag = "bP")
-                            if (thisPiece.symbol == 'ro'):
-                                self.myCanvas.create_image(i*M+M/2, j*M+M/2, image = self.whiteRook, tag="bR")
-                            if (thisPiece.symbol == 'kn'):
-                                self.myCanvas.create_image(i*M+M/2, j*M+M/2, image = self.whiteKnight, tag="bN")
-                            if (thisPiece.symbol == 'bi'):
-                                self.myCanvas.create_image(i*M+M/2, j*M+M/2, image = self.whiteBishop, tag="bB")
-                            if (thisPiece.symbol == 'Qu'):
-                                self.myCanvas.create_image(i*M+M/2, j*M+M/2, image = self.whiteQueen, tag="bQ")
-                            if (thisPiece.symbol == 'Ki'):
-                                self.myCanvas.create_image(i*M+M/2, j*M+M/2, image = self.whiteKing, tag="bK")        
-            self.myCanvas.addtag_all("all")    
+        M = self.M
+        for i in range(self.SQUARES):
+            for j in range(self.SQUARES):
+                thisPiece = self.myGame.getPiece(Square(i,j))
+                if (thisPiece):
+                    if (thisPiece.color == 0):
+                        if (thisPiece.symbol == 'pa'):
+                            self.myCanvas.create_image(i*M+M/2, j*M+M/2, image = self.blackPawn, tag = "bP")
+                        if (thisPiece.symbol == 'ro'):
+                            self.myCanvas.create_image(i*M+M/2, j*M+M/2, image = self.blackRook, tag="bR")
+                        if (thisPiece.symbol == 'kn'):
+                            self.myCanvas.create_image(i*M+M/2, j*M+M/2, image = self.blackKnight, tag="bN")
+                        if (thisPiece.symbol == 'bi'):
+                            self.myCanvas.create_image(i*M+M/2, j*M+M/2, image = self.blackBishop, tag="bB")
+                        if (thisPiece.symbol == 'Qu'):
+                            self.myCanvas.create_image(i*M+M/2, j*M+M/2, image = self.blackQueen, tag="bQ")
+                        if (thisPiece.symbol == 'Ki'):
+                            self.myCanvas.create_image(i*M+M/2, j*M+M/2, image = self.blackKing, tag="bK")
+                    if (thisPiece.color == 1):
+                        if (thisPiece.symbol == 'pa'):
+                            self.myCanvas.create_image(i*M+M/2, j*M+M/2, image = self.whitePawn, tag = "bP")
+                        if (thisPiece.symbol == 'ro'):
+                            self.myCanvas.create_image(i*M+M/2, j*M+M/2, image = self.whiteRook, tag="bR")
+                        if (thisPiece.symbol == 'kn'):
+                            self.myCanvas.create_image(i*M+M/2, j*M+M/2, image = self.whiteKnight, tag="bN")
+                        if (thisPiece.symbol == 'bi'):
+                            self.myCanvas.create_image(i*M+M/2, j*M+M/2, image = self.whiteBishop, tag="bB")
+                        if (thisPiece.symbol == 'Qu'):
+                            self.myCanvas.create_image(i*M+M/2, j*M+M/2, image = self.whiteQueen, tag="bQ")
+                        if (thisPiece.symbol == 'Ki'):
+                            self.myCanvas.create_image(i*M+M/2, j*M+M/2, image = self.whiteKing, tag="bK")        
+        self.myCanvas.addtag_all("all")    
 
